36016f48113ea499fcd0aceee098a2dbe66380b8
Spacyes241 <farrelhaykalindonesia@gmail.com>
2024-03-03 12:21:54 +0700

    Third commit

--- Jurnal03_1302220064/Form1.Designer.cs
+++ Jurnal03_1302220064/Form1.Designer.cs
@@ -102,7 +102,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
